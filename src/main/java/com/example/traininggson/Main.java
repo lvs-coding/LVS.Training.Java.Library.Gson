8577b795f118d5e6948a4bf9f197b5b140a7a3d6
livos <lvivancos@gmail.com>
2017-07-06 15:03:00 +0700

    Test code removed

--- src/main/java/com/example/traininggson/Main.java
+++ src/main/java/com/example/traininggson/Main.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 public class Main    {
 	public static void main(String[] args) throws IOException {
-		TestBidon testBidon = new TestBidon();
 		
 		TranslationRequest request = new TranslationRequest();
 		request.translate("sắt", "vie", "eng");
